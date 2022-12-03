5d49eeefbe3137dac1f857525b736b4691fda59c
Matheus.Sakuragui <83602968+MatheusSakuragui@users.noreply.github.com>
2022-12-03 19:39:25 -0300

    Rota para deletar usuario

--- atviii-autobots/automanager/src/main/java/com/autobots/automanager/controles/UsuarioControler.java
+++ atviii-autobots/automanager/src/main/java/com/autobots/automanager/controles/UsuarioControler.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,9 +21,13 @@ import com.autobots.automanager.entitades.Email;
 import com.autobots.automanager.entitades.Empresa;
 import com.autobots.automanager.entitades.Telefone;
 import com.autobots.automanager.entitades.Usuario;
+import com.autobots.automanager.entitades.Veiculo;
+import com.autobots.automanager.entitades.Venda;
 import com.autobots.automanager.link.UsuarioLink;
 import com.autobots.automanager.servicos.EmpresaServico;
 import com.autobots.automanager.servicos.UsuarioServico;
+import com.autobots.automanager.servicos.VeiculoServico;
+import com.autobots.automanager.servicos.VendaServico;
 
 @RestController
 @RequestMapping("/usuario")
@@ -35,7 +40,12 @@ public class UsuarioControler {
 	private EmpresaServico empresaServico;
 	
 	@Autowired
+	private VendaServico vendaServico;
 	
+	@Autowired
+	private VeiculoServico veiculoServico;
+	
+	@Autowired
 	private UsuarioLink usuarioLink;
 	
 	@PostMapping("/cadastrar/{id}")
@@ -92,4 +102,54 @@ public class UsuarioControler {
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 	
+	/**
+	 * @param id
+	 * @return
+	 */
+	@DeleteMapping("/deletar/{id}")
+	public ResponseEntity<?> deletar(@PathVariable Long id){
+		Usuario usuarioSelecionado = usuarioServico.pegarPeloId(id);
+		if (usuarioSelecionado == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		for (Empresa empresa : empresaServico.pegarTodasEmpresas()) {
+			for(Usuario funcionario : empresa.getUsuarios()) {
+				if(funcionario.getId() == usuarioSelecionado.getId()) {
+					empresa.getUsuarios().remove(funcionario);
+				}
+			}
+		}
+		
+		 for(Venda venda : vendaServico.pegarTodasVendas()) {
+			 if(venda.getFuncionario().getId() == usuarioSelecionado.getId()) {
+				 venda.setFuncionario(null);
+			 }
+			 if(venda.getCliente().getId() == usuarioSelecionado.getId()) {
+				 venda.setCliente(null);
+			 }
+		 }
+		
+		for (Veiculo veiculo : veiculoServico.pegarTodosVeiculos()) {
+			if(veiculo.getProprietario().getId() == usuarioSelecionado.getId()) {
+				veiculo.setProprietario(null);
+			}
+		}
+		
+		usuarioSelecionado.getDocumentos().removeAll(usuarioSelecionado.getDocumentos());
+		usuarioSelecionado.getTelefones().removeAll(usuarioSelecionado.getTelefones());
+		usuarioSelecionado.getEmails().removeAll(usuarioSelecionado.getEmails());
+		usuarioSelecionado.getCredenciais().removeAll(usuarioSelecionado.getCredenciais());
+		usuarioSelecionado.getMercadorias().removeAll(usuarioSelecionado.getMercadorias());
+		usuarioSelecionado.getVeiculos().removeAll(usuarioSelecionado.getVeiculos());
+		usuarioSelecionado.getVendas().removeAll(usuarioSelecionado.getVendas());
+		usuarioSelecionado.setEndereco(null);
+		
+		
+		usuarioServico.deletarUsuario(usuarioSelecionado);
+		
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
+			
+		}
+
 }
